263484a1f429dde9e0d6680a94b03a409bb5b660
veran <veran@DESKTOP-QC3MR3R>
2021-12-09 14:40:22 +0200

    First test(100 numbers) pass

--- FizzBuzzKata/src/FizzBuzz.java
+++ FizzBuzzKata/src/FizzBuzz.java
@@ -1,7 +1,7 @@
 public class FizzBuzz {
 
 	public static void main(String[] args) {
-        for (int i = 1; i < 100; i++){
+        for (int i = 1; i < 101; i++){
             System.out.println(of(i));
         }
     }
